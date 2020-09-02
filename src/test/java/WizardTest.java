da6a11873679c8f61febcc1533478300994e6132
Helena <inger.helena@gmail.com>
2020-09-02 14:25:14 +0100

    Wizard treasureCount, addHealth, removeHealth, addTreasure, removeTreasure tests pass

--- src/test/java/WizardTest.java
+++ src/test/java/WizardTest.java
@@ -1,4 +1,6 @@
 import components.Companion;
+import components.Treasure;
+import components.TreasureValue;
 import org.junit.Before;
 import org.junit.Test;
 import players.magic.Wizard;
@@ -9,11 +11,13 @@ public class WizardTest {
 
     Wizard wizard;
     Companion companion;
+    Treasure treasure;
 
     @Before
     public void before() {
         companion = new Companion("Hoot", "owl", 2);
         wizard = new Wizard(100, 9, companion);
+        treasure = new Treasure("Ruby", TreasureValue.RARE);
     }
 
     @Test
@@ -29,5 +33,35 @@ public class WizardTest {
     @Test
     public void canGetCompanion() {
         assertEquals(companion, wizard.getCompanion());
+    }
+
+    @Test
+    public void canGetTreasureCount() {
+        assertEquals(0, wizard.getTreasureCount());
+    }
+
+    @Test
+    public void canAddTreasure() {
+        wizard.addTreasure(treasure);
+        assertEquals(1, wizard.getTreasureCount());
+    }
+
+    @Test
+    public void canRemoveTreasure() {
+        wizard.addTreasure(treasure);
+        wizard.removeTreasure(treasure);
+        assertEquals(0, wizard.getTreasureCount());
+    }
+
+    @Test
+    public void canAddHealth() {
+        wizard.addHealth(10);
+        assertEquals(110, wizard.getHealth());
+    }
+
+    @Test
+    public void canRemoveHealth() {
+        wizard.removeHealth(10);
+        assertEquals(90, wizard.getHealth());
     }
 }
