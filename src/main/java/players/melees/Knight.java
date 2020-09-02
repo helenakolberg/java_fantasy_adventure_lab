0a80703d2e875778f52882e422e8c78372cfc580
Helena <inger.helena@gmail.com>
2020-09-02 13:26:31 +0100

    created Knight class, canGetHealth, canGetStrength, canGetEquippedWeapon passing

--- src/main/java/players/melees/Knight.java
+++ src/main/java/players/melees/Knight.java
@@ -1,4 +1,10 @@
 package players.melees;
 
-public class Knight {
+import components.Weapon;
+
+public class Knight extends Melee {
+
+    public Knight(int health, int strength, Weapon equippedWeapon) {
+        super(health, strength, equippedWeapon);
+    }
 }
